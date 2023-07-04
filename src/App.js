f59b375080e157ed0b1a3d97c3eeec53e51531ac
Javesshhh <javeshlodha02@gmail.com>
2023-07-05 01:25:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/App.scss";
+import "./Styles/intro.scss";
+import "./Styles/section.scss";
+import "./Styles/footer.scss";
+import "./Styles/misc.scss";
+import "./Styles/mediaQuery.scss";
+import { useEffect, useState } from "react";
+import Footer from "./Components/Footer";
+
+import IntroVideo from "./Components/IntroVideo";
+import Section from "./Components/Section";
+import freshTopicSrc from "./Assets/academy.png";
+import data from "./Data/data.json";
+import freshTopic2Src from "./Assets/story.png";
+import tedTalksSrc from "./Assets/in-the-news.gif";
+import franchiseSrc from "./Assets/franchise.gif";
+import mapSrc from "./Assets/locations.png";
+import coursesSrc from "./Assets/image2.png";
+import albumSrc from "./Assets/mba-cares.gif";
+import baratSrc from "./Assets/image1.png";
+import chaiwalaSrc from "./Assets/image3.png";
+import Misc from "./Components/Misc";
+
+const yellow = "#fff21A",
+  pink = "#ed1e79",
+  white = "#fff",
+  brown = "#6d3d0f";
 
 function App() {
+  const {
+    freshTopic,
+    freshTopic2,
+    tedTalks,
+    franchise,
+    map,
+    courses,
+    album,
+    barat,
+    chaiwala,
+  } = data;
+
+  const [loading, setLoading] = useState(true);
+
+  const dotCursor = (e) => {
+    const cursor = document.querySelector(".cursor");
+    cursor.style.top = `${e.pageY - 14}px`;
+    cursor.style.left = `${e.pageX - 14}px`;
+
+    const element = e.target;
+
+    if (element.getAttribute("data-cursorpointer")) {
+      cursor.classList.add("cursorHover");
+    } else if (element.getAttribute("data-cursorpointermini")) {
+      cursor.classList.add("cursorHoverMini");
+    } else {
+      cursor.classList.remove("cursorHover");
+      cursor.classList.remove("cursorHoverMini");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", dotCursor);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => {
+      window.removeEventListener("mousemove", dotCursor);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <IntroVideo />
+      <Section
+        h3={freshTopic.heading}
+        text={freshTopic.text}
+        btnTxt={freshTopic.btn}
+        imgSrc={freshTopicSrc}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
+      <Section
+        h3={freshTopic2.heading}
+        text={freshTopic2.text}
+        btnTxt={freshTopic2.btn}
+        imgSrc={freshTopic2Src}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
+      <Section
+        h3={tedTalks.heading}
+        text={tedTalks.text}
+        btnTxt={tedTalks.btn}
+        imgSrc={tedTalksSrc}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
+      {/* Franchise */}
+      <Section
+        h3={franchise.heading}
+        text={franchise.text}
+        btnTxt={franchise.btn}
+        imgSrc={franchiseSrc}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />{" "}
+      {/* Map */}
+      <Section
+        h3={map.heading}
+        text={map.text}
+        imgSrc={mapSrc}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={brown}
+        btnColor={yellow}
+        hasBtn={false}
+      />{" "}
+      {/* Courses */}
+      <Section
+        h3={courses.heading}
+        text={courses.text}
+        btnTxt={courses.btn}
+        imgSrc={coursesSrc}
+        imgSize={"30%"}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
+      {/* Album */}
+      <Section
+        h3={album.heading}
+        text={album.text}
+        btnTxt={album.btn}
+        imgSrc={albumSrc}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />{" "}
+      {/* Barat */}
+      <Section
+        h3={barat.heading}
+        text={barat.text}
+        btnTxt={barat.btn}
+        imgSrc={baratSrc}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />{" "}
+      {/* ChaiWala */}
+      <Section
+        h3={chaiwala.heading}
+        text={chaiwala.text}
+        btnTxt={chaiwala.btn}
+        imgSrc={chaiwalaSrc}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
+      <Footer />
+      <Misc />
+    </>
   );
 }
 
